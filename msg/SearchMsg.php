f0aa3f60189167760635a66fdfca33c85fb7c7eb
petrov86 <t_petrov@mail.bg>
2013-08-17 11:46:19 +0300

    Create SearchMsg.php

---
+++ msg/SearchMsg.php
@@ -0,0 +1,43 @@
+<?php
+//session_start();
+require_once "database.php";
+
+
+$DateTimeNow =  date( 'Y-m-d H:i:s');
+
+function ShowSearchedMsg ($DateFrom, $DateTo)
+		{	
+			
+			$sql = "SELECT messages.msgID, messages.message, messages.message_time, users.username FROM messages 
+					JOIN users ON messages.userID= users.id WHERE messages.message_time >= '$DateFrom' AND messages.message_time <= '$DateTo'
+					ORDER BY messages.msgID DESC";
+			$res = mysql_query($sql);
+			$stringResult ="";
+			while ($row = mysql_fetch_assoc($res)) {	
+					$stringResult = $stringResult."<div><p>From: <b>".$row["username"].", </b>Posted at: ". $row["message_time"]."</p><h4><pre>".$row["message"]."</pre></h4></div><hr/>";	
+			}
+
+			//$stringResult=iconv("UTF-8",$stringResult);
+			$stringResult = html_entity_decode($stringResult); 
+			echo $stringResult;				
+			//echo json_encode($stringResult);
+		}
+		
+$DateFrom = '1970-01-01';
+$DateTo = date( 'Y-m-d H:i:s');
+
+if (isset($_POST["DateFrom"]))
+  { 	 	
+  		$DateFrom = $_POST["DateFrom"]; 		
+  }		
+
+if (isset($_POST["DateTo"]))
+  { 	 
+  		if($_POST["DateTo"] != date('Y-m-d'))
+  		{
+  			$DateTo = $_POST["DateTo"]. '24:0:0'; 	
+  		}	
+  		
+  }		
+
+ShowSearchedMsg ($DateFrom, $DateTo);
