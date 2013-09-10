f73cedd5752910595e7d4e85e700b9c1618d0cbc
petrov86 <t_petrov@mail.bg>
2013-09-10 16:47:27 +0300

    Update EditMsg.php

--- msg/EditMsg.php
+++ msg/EditMsg.php
@@ -1,25 +1,48 @@
 <?php
 require_once "database.php";
+require_once "functions.php";
 
-function DelMsgForm()
+$DateTimeNow =  date( 'Y-m-d H:i:s');
+function DelMsgForm($DateFrom, $DateTo)
 		{	
 			
 			$sql = "SELECT messages.msgID, messages.message, messages.message_time, users.username FROM messages 
-					JOIN users ON messages.userID= users.id 
+					JOIN users ON messages.userID= users.id WHERE messages.message_time >= '$DateFrom' AND messages.message_time <= '$DateTo'
 					ORDER BY messages.msgID DESC";
 			$res = mysql_query($sql);
+			$stringResult ="";
 			echo "<form method='post' action=''>";
-			while ($row = mysql_fetch_assoc($res)) {			
-				echo "<div>";
-				echo "<input type='checkbox' value=". $row["msgID"]. " name=".$row["msgID"]."> From: <b>" . $row["username"] . ",</b>  "  
-				."Posted at: " . $row["message_time"];	
-				echo "<h4>".$row["message"]."</h4>";				
-				echo "</div>";
-				echo "<hr />";
+			while ($row = mysql_fetch_assoc($res)) 
+			{
+					if(filter_var($row["message"], FILTER_VALIDATE_URL))
+							{	
+								$row["message"] = makeClickableLinks($row["message"]);
+							}
+					$stringResult = $stringResult."<div><input type='checkbox' value=". $row["msgID"]. " name=".$row["msgID"]."> From: <b>" . $row["username"] . ",</b>  "  
+					."Posted at: " . $row["message_time"]."<h4>".$row["message"]."</h4></div><hr />";
 			}
-			echo "<input type='submit' value='Delete Message' name='del' class='buttonBig'>";
-			echo "</form>";
-			
+			$stringResult = html_entity_decode($stringResult); 
+			echo $stringResult;	
+			echo "<input type='submit' value='Delete Messages' name='del' class='buttonBig'>";
+			echo "</form>";	
 		}
 		
-	DelMsgForm();
+
+$DateFrom = '1970-01-01';
+$DateTo = date( 'Y-m-d H:i:s');	
+
+if (isset($_POST["DateFrom"]))
+  { 	 	
+  		$DateFrom = $_POST["DateFrom"]; 		
+  }		
+
+if (isset($_POST["DateTo"]))
+  { 	 
+  		if($_POST["DateTo"] != date('Y-m-d'))
+  		{
+  			$DateTo = $_POST["DateTo"]. '24:0:0'; 	
+  		}	
+  		
+  }		
+
+DelMsgForm ($DateFrom, $DateTo);
