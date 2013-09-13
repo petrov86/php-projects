3a58888421f46860928e12ad6a51ba0965357bfe
petrov86 <t_petrov@mail.bg>
2013-09-13 12:59:13 +0300

    Update EditMsg.php

--- msg/EditMsg.php
+++ msg/EditMsg.php
@@ -3,11 +3,14 @@ require_once "database.php";
 require_once "functions.php";
 
 $DateTimeNow =  date( 'Y-m-d H:i:s');
-function DelMsgForm($DateFrom, $DateTo)
+
+
+function DelMsgForm($DateFrom, $DateTo, $messageStr)
 		{	
 			
 			$sql = "SELECT messages.msgID, messages.message, messages.message_time, users.username FROM messages 
-					JOIN users ON messages.userID= users.id WHERE messages.message_time >= '$DateFrom' AND messages.message_time <= '$DateTo'
+					JOIN users ON messages.userID= users.id 
+					WHERE messages.message_time >= '$DateFrom' AND messages.message_time <= '$DateTo' AND messages.message LIKE '%$messageStr%' 
 					ORDER BY messages.msgID DESC";
 			$res = mysql_query($sql);
 			$stringResult ="";
@@ -29,20 +32,27 @@ function DelMsgForm($DateFrom, $DateTo)
 		
 
 $DateFrom = '1970-01-01';
-$DateTo = date( 'Y-m-d H:i:s');	
-
+$DateTo = date( 'Y-m-d H:i:s');
+$messageStr="";		
+		
+if (isset($_POST["messageStr"]))
+	{ 	 	
+		$messageStr = $_POST["messageStr"]; 		
+	}		
 if (isset($_POST["DateFrom"]))
-  { 	 	
-  		$DateFrom = $_POST["DateFrom"]; 		
-  }		
+	{ 	 	
+		$DateFrom = $_POST["DateFrom"]; 		
+	}		
 
 if (isset($_POST["DateTo"]))
-  { 	 
+	{ 	 
   		if($_POST["DateTo"] != date('Y-m-d'))
   		{
   			$DateTo = $_POST["DateTo"]. '24:0:0'; 	
   		}	
   		
-  }		
+	}		
+  
 
-DelMsgForm ($DateFrom, $DateTo);
+DelMsgForm ($DateFrom, $DateTo, $messageStr);
+
